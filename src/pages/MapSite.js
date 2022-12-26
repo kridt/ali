ad1358dd670840277a163cb3c3199eab2d4c704a
Christian <chrnielsen2003@gmail.com>
2022-12-26 05:23:00 +0100

    date counter

--- src/pages/MapSite.js
+++ src/pages/MapSite.js
@@ -30,50 +30,30 @@ export default function MapSite() {
     googleMapsApiKey: process.env.REACT_APP_GOOGLEMAPS,
   });
 
-  const [setMap] = React.useState(null);
-
-  /*  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map);
-  }, []); */
-
-  const onUnmount = React.useCallback(
-    function callback(map) {
-      setMap(null);
-    },
-    [setMap]
-  );
-
-  useEffect(() => {
-    var d = new Date();
-    var test = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
-    console.log(test);
-  }, []);
+  function clickedMarker(e, item) {
+    console.log(item);
+  }
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      /* onLoad={onLoad} */
-      onUnmount={onUnmount}
-      zoom={7}
-    >
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={7}>
       {/* Child components, such as markers, info windows, etc. */}
       <div>
         {allKunder?.map((item) => {
-          const dayCount = item.date.split("-")[2];
-          const monthCount = item.date.split("-")[1];
+          const today = new Date().getTime();
+          const lastService = new Date(item.date).getTime();
+          const diff = today - lastService;
+          const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+
           return (
-            <Marker
-              key={item.telefon}
-              position={item.geoLocation}
-              animation={2}
-              label={item.service}
-              onClick={(e) => console.log(e.latLng.toJSON())}
-            />
+            <>
+              <Marker
+                key={item.telefon}
+                position={item.geoLocation}
+                animation={2}
+                label={`${365 - days}`}
+                onClick={(e) => clickedMarker(e, item)}
+              />
+            </>
           );
         })}
       </div>
